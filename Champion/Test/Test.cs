f273477711b77d17a9325b9b633b3a66a9217102
oktwme <105168775+oktwme@users.noreply.github.com>
2023-04-02 06:25:48 -0700

    Update Test.cs

--- Champion/Test/Test.cs
+++ Champion/Test/Test.cs
@@ -1,32 +1,69 @@
-﻿using System;
+using EnsoulSharp;
+using EnsoulSharp.SDK;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using EnsoulSharp;
-using EnsoulSharp.SDK;
-using EnsoulSharp.SDK.MenuUI;
-using RankerAIO.Common;
 
-namespace RankerAIO.Champion.Test
+namespace XerathExploit
 {
-    class Test : Base
+    class Program
     {
-        public static void Init()
-        {
-            ChampionMenu = new Menu("Test", "Test", true).Attach();
-            Korean = ChampionMenu.Add(new MenuBool("Korean", "Korean")).Enabled;
+        private static AIHeroClient Player;
+        private static Spell Q, W, E, R;
 
-            Game.OnUpdate += Game_OnUpdate;
+        static void Main(string[] args)
+        {
+            GameEvent.OnGameLoad += OnLoad;
         }
 
-        private static void Game_OnUpdate(EventArgs args)
+        private static void OnLoad()
         {
-            if(Orbwalker.ActiveMode == OrbwalkerMode.Harass)
-            {
-                for (int i = 0; i < Player.Buffs.Length; i++) Console.WriteLine(i + " : " + Player.Buffs[i].Name);
+            Player = ObjectManager.Player;
+            Q = new Spell(SpellSlot.Q, 1600f);
+            W = new Spell(SpellSlot.W, 1000f);
+            E = new Spell(SpellSlot.E, 1050f);
+            R = new Spell(SpellSlot.R, 5600f);
 
-                //Console.WriteLine(Player.HasBuff("SivirE"));
+            Q.SetSkillshot(0.6f, 100f, 1000f, false, SpellType.Line);
+            W.SetSkillshot(0.25f, 250f, float.MaxValue, false, SpellType.Circle);
+            E.SetSkillshot(0.25f, 60f, 1400f, true, SpellType.Line);
+            R.SetSkillshot(0.7f, 100f, float.MaxValue, false, SpellType.Circle);
+
+            EnsoulSharp.SDK.Events.Tick.OnTick += OnTick;
+        }
+
+        private static void OnTick(EventArgs args)
+        {
+            if (Player.IsDead) return;
+            if (Orbwalker.ActiveMode == OrbwalkerMode.None) return;
+
+            var target = TargetSelector.GetTarget(Q.Range);
+            if (target == null) return;
+
+            var qPrediction = Q.GetPrediction(target);
+            if (qPrediction.Hitchance >= HitChance.High)
+            {
+                Q.Cast(qPrediction.CastPosition);
+            }
+
+            var wPrediction = W.GetPrediction(target);
+            if (wPrediction.Hitchance >= HitChance.High)
+            {
+                W.Cast(wPrediction.CastPosition);
+            }
+
+            var ePrediction = E.GetPrediction(target);
+            if (ePrediction.Hitchance >= HitChance.High)
+            {
+                E.Cast(ePrediction.CastPosition);
+            }
+
+            var rPrediction = R.GetPrediction(target);
+            if (rPrediction.Hitchance >= HitChance.High)
+            {
+                R.Cast(rPrediction.CastPosition);
             }
         }
     }
