b362f838ce4443687f81af81d0ddc202e31fdfb9
oktwme <105168775+oktwme@users.noreply.github.com>
2023-04-02 09:54:07 -0700

    Update Test.cs

--- Champion/Test/Test.cs
+++ Champion/Test/Test.cs
@@ -1,69 +1,41 @@
 using EnsoulSharp;
 using EnsoulSharp.SDK;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using EnsoulSharp.SDK.Utility;
 
-namespace XerathExploit
+namespace MyPlugin
 {
-    class Program
+    public class Plugin
     {
-        private static AIHeroClient Player;
-        private static Spell Q, W, E, R;
-
-        static void Main(string[] args)
+        public Plugin()
         {
             GameEvent.OnGameLoad += OnLoad;
         }
 
-        private static void OnLoad()
+        private void OnLoad()
         {
-            Player = ObjectManager.Player;
-            Q = new Spell(SpellSlot.Q, 1600f);
-            W = new Spell(SpellSlot.W, 1000f);
-            E = new Spell(SpellSlot.E, 1050f);
-            R = new Spell(SpellSlot.R, 5600f);
+            Chat.Print("MyPlugin loaded!");
 
-            Q.SetSkillshot(0.6f, 100f, 1000f, false, SpellType.Line);
-            W.SetSkillshot(0.25f, 250f, float.MaxValue, false, SpellType.Circle);
-            E.SetSkillshot(0.25f, 60f, 1400f, true, SpellType.Line);
-            R.SetSkillshot(0.7f, 100f, float.MaxValue, false, SpellType.Circle);
-
-            EnsoulSharp.SDK.Events.Tick.OnTick += OnTick;
+            GameEvent.OnUpdate += OnUpdate;
         }
 
-        private static void OnTick(EventArgs args)
+        private void OnUpdate(EventArgs args)
         {
-            if (Player.IsDead) return;
-            if (Orbwalker.ActiveMode == OrbwalkerMode.None) return;
+            var player = ObjectManager.Player;
 
-            var target = TargetSelector.GetTarget(Q.Range);
-            if (target == null) return;
-
-            var qPrediction = Q.GetPrediction(target);
-            if (qPrediction.Hitchance >= HitChance.High)
+            if (player.ChampionName == "Ezreal")
             {
-                Q.Cast(qPrediction.CastPosition);
-            }
+                var target = TargetSelector.GetTarget(player.Spellbook.GetSpell(SpellSlot.Q).SData.CastRange);
 
-            var wPrediction = W.GetPrediction(target);
-            if (wPrediction.Hitchance >= HitChance.High)
-            {
-                W.Cast(wPrediction.CastPosition);
-            }
+                if (target != null)
+                {
+                    var prediction = Prediction.GetPrediction(player.Spellbook.GetSpell(SpellSlot.Q), target);
 
-            var ePrediction = E.GetPrediction(target);
-            if (ePrediction.Hitchance >= HitChance.High)
-            {
-                E.Cast(ePrediction.CastPosition);
-            }
-
-            var rPrediction = R.GetPrediction(target);
-            if (rPrediction.Hitchance >= HitChance.High)
-            {
-                R.Cast(rPrediction.CastPosition);
+                    if (prediction.Hitchance >= HitChance.High)
+                    {
+                        var packet = Packet.C2S.Cast.Create(player.Spellbook.GetSpell(SpellSlot.Q).Slot, prediction.CastPosition);
+                        packet.Send();
+                    }
+                }
             }
         }
     }
